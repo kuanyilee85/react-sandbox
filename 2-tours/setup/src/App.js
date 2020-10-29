7e582387b49a392ccfb76482bd01fbb304835887
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-29 09:36:56 -0400

    project 2-tours, fetch data

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -4,7 +4,39 @@ import Tours from './Tours';
 const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchUrl = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
 export default App;
