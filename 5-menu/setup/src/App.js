46c1979df690329d7f2dfd46a29fdce22cc03a9b
kuanyilee85 <kuanyilee85@gmail.com>
2020-11-01 10:30:19 -0500

    manually render menu button

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -6,6 +6,14 @@ import items from './data';
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+  const filterItems = (category) => {
+    const newItem = items.filter((item) => item.category === category);
+    if (category === 'all') {
+      return setMenuItems(items);
+    }
+    setMenuItems(newItem);
+  };
+
   return (
     <main>
       <section className="menu section">
@@ -13,7 +21,7 @@ function App() {
           <h2>Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
