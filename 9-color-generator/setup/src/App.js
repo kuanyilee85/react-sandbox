30a2d0104bb4002ba2cddca4ddcba68341aa8049
kuanyilee85 <kuanyilee85@gmail.com>
2020-11-04 23:26:19 -0500

    9-color generator

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -6,23 +6,31 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
     <>
       <section className="container">
-        <h3>color generator</h3>
+        <h3>generate your color palette:</h3>
         <form onClick={handleSubmit}>
           <input
             type="text"
             value={color}
             onChange={(e) => setColor(e.target.value)}
             placeholder="#f15025"
+            className={`${error ? 'error' : null}`}
           />
           <button className="btn" type="submit">
             Submit
@@ -30,7 +38,16 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
       </section>
     </>
   );
