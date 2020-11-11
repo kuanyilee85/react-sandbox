3e620c9712241b5504afd95631d18d768fa5d468
kuanyilee85 <kuanyilee85@gmail.com>
2020-11-10 22:39:40 -0500

    15-cocktail

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat,
+        temporibus, doloribus ipsum quisquam eveniet laudantium ullam officiis
+        nisi dolore et assumenda suscipit laboriosam rem est illum sunt?
+        Consequuntur voluptatem, at cupiditate aperiam libero facere deleniti
+        porro architecto incidunt perferendis. Eius!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
